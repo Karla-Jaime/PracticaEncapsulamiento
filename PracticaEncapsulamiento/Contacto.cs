dfadef6b63be83d9d49c46da571bc743b98f87e2
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-14 08:32:23 -0700

    Agregar archivos de proyecto.

---
+++ PracticaEncapsulamiento/Contacto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaEncapsulamiento
+{
+    class Contacto
+    {
+        //Auto propertires
+        //Cuando se declara una variable utilizando properties usar mayuscular para el nombre de la variable
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string TelefonoCasa { get; set; }
+        public string CorreoElectronico { get; set; }
+
+        //Properties
+        private string _telefonoCelular;// Variable de respaldo (se usa el _ para identificarlo como tal)
+        public string TelefonoCelular
+        {
+            get {
+                return _telefonoCelular;
+            }
+            set{
+                if (value.Length == 10)
+                {
+                    _telefonoCelular = value;
+                }
+                else if (value.Length == 7)
+                {
+                    _telefonoCelular = "644" + value;
+                }
+                else
+                {
+                    Console.WriteLine("Introduce un número de celular válido");
+                }
+            }
+        } 
+    }
+}
---
+++ PracticaEncapsulamiento/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaEncapsulamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creamos una instancia de contacto
+            Contacto contactoUno = new Contacto();
+
+            contactoUno.Nombre = 
+                "Maria Antonieta de las Nieves";
+            contactoUno.Direccion =
+                "Bahía de Kino 1211";
+            contactoUno.CorreoElectronico = 
+                "chilindrina-lentitos@el-chavo.com";
+            contactoUno.TelefonoCasa = 
+                "4-35-78-15";
+            contactoUno.TelefonoCelular = 
+                "64418517889";
+
+            Console.WriteLine("Nombre: " + contactoUno.Nombre);
+            Console.WriteLine("Dirección: " + contactoUno.Direccion);
+            Console.WriteLine("Correo: " + contactoUno.CorreoElectronico );
+            Console.WriteLine("Telefono de Casa: " + contactoUno.TelefonoCasa);
+            Console.WriteLine("Telefono Celular: " + contactoUno.TelefonoCelular);
+
+            Console.ReadLine();
+        }
+    }
+}
